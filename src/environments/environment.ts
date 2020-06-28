5f50eadeaa8663d86097018795a087c022d54f2c
kusumoto <gkusumoto@gmail.com>
2020-06-28 23:23:46 +0900

    first commit (developing)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDJC0Lff0E9Kwb-zBBJOSIcTK6zlgO_Zcw",
+    authDomain: "sample-c7937.firebaseapp.com",
+    databaseURL: "https://sample-c7937.firebaseio.com",
+    projectId: "sample-c7937",
+    storageBucket: "sample-c7937.appspot.com",
+    messagingSenderId: "713103490521",
+    appId: "1:713103490521:web:fb5d0ddd9d972a1408edc8",
+    measurementId: "G-0RF1D8P14W"
+  }
 };
 
 /*
